6fedb3f53a7027207afefa94177d0d321175671f
oldrandom <142756965+oldrandom@users.noreply.github.com>
2025-11-27 11:04:59 -0500

    Add user configuration for startpage settings

---
+++ userconfig.js
@@ -0,0 +1,247 @@
+// User configuration for the startpage - update the palette, location, and your preferred tabs, categories, and links
+
+// Define preferred palette for light and dark mode
+// Available themes: latte, frappe, mocha, macchiato
+const preferredLightTheme = latte;
+const preferredDarkTheme = mocha;
+
+let palette = initThemeSystem(preferredLightTheme, preferredDarkTheme);
+
+const default_configuration = {
+  overrideStorage: true,
+  temperature: {
+    location: "London",
+    scale: "C",
+  },
+  clock: {
+    format: "k:i p",
+    icon_color: palette.maroon,
+  },
+  additionalClocks: [
+    {
+      label: "UA",
+      timezone: "Europe/Kyiv",
+      format: "h:i",
+      icon_color: palette.peach,
+    },
+  ],
+  search: {
+    engines: {
+      p: ["https://www.perplexity.ai/search/?q=", "PerplexityAI"],
+      d: ["https://duckduckgo.com/?q=", "DuckDuckGo"],
+      g: ["https://google.com/search?q=", "Google"],
+    },
+    default: "d",
+  },
+  keybindings: {
+    "s": "search-bar",
+  },
+  disabled: [],
+  localIcons: true,
+  localFonts: true,
+  fastlink: "https://www.perplexity.ai",
+  openLastVisitedTab: true,
+  tabs: [
+    {
+      name: "myself",
+      background_url: "src/img/banners/banner_09.gif",
+      categories: [
+        {
+          name: "bookmarks",
+          links: [
+            {
+              name: "github",
+              url: "https://github.com",
+              icon: "brand-github",
+              icon_color: palette.green,
+            },
+            {
+              name: "discord",
+              url: "https://canary.discord.com/app",
+              icon: "brand-discord",
+              icon_color: palette.blue,
+            },
+          ],
+        },
+        {
+          name: "workspace",
+          links: [
+            {
+              name: "gmail",
+              url: "https://mail.google.com",
+              icon: "brand-gmail",
+              icon_color: palette.green,
+            },
+            {
+              name: "calendar",
+              url: "https://calendar.google.com",
+              icon: "calendar-filled",
+              icon_color: palette.peach,
+            },
+            {
+              name: "sheets",
+              url: "https://docs.google.com/spreadsheets",
+              icon: "table",
+              icon_color: palette.red,
+            },
+            {
+              name: "drive",
+              url: "https://drive.google.com/drive/home",
+              icon: "brand-google-drive",
+              icon_color: palette.blue,
+            },
+          ],
+        },
+        {
+          name: "media",
+          links: [
+            {
+              name: "new york times",
+              url: "https://nytimes.com",
+              icon: "news",
+              icon_color: palette.blue,
+            },
+            {
+              name: "hackernews",
+              url: "https://news.ycombinator.com",
+              icon: "brand-redhat",
+              icon_color: palette.peach,
+            },
+            {
+              name: "youtube",
+              url: "https://www.youtube.com/",
+              icon: "border-radius",
+              icon_color: palette.red,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "dev",
+      background_url: "src/img/banners/banner_15.gif",
+      categories: [
+        {
+          name: "development",
+          links: [
+            {
+              name: "github",
+              url: "https://github.com/oldrandom?tab=repositories",
+              icon: "brand-github-copilot",
+              icon_color: palette.green,
+            },
+            {
+              name: "stackoverflow",
+              url: "https://stackoverflow.com",
+              icon: "brand-stackoverflow",
+              icon_color: palette.red,
+            },
+          ],
+        },
+        {
+          name: "challenges",
+          links: [
+            {
+              name: "leetcode",
+              url: "https://leetcode.com",
+              icon: "code-plus",
+              icon_color: palette.peach,
+            },
+          ],
+        },
+        {
+          name: "resources",
+          links: [
+            {
+              name: "first",
+              url: "https://frcdocs.wpi.edu/en/2025/index.html",
+              icon: "robot",
+              icon_color: palette.peach,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "chi ll",
+      background_url: "src/img/banners/banner_08.gif",
+      categories: [
+        {
+          name: "social media",
+          links: [
+            {
+              name: "telegram",
+              url: "https://web.telegram.org",
+              icon: "brand-telegram",
+              icon_color: palette.green,
+            },
+            {
+              name: "reddit",
+              url: "https://www.reddit.com/r/unixporn",
+              icon: "brand-reddit",
+              icon_color: palette.red,
+            },
+            {
+              name: "discord",
+              url: "https://canary.discord.com/app",
+              icon: "brand-discord",
+              icon_color: palette.blue,
+            },
+          ],
+        },
+        {
+          name: "gaming",
+          links: [
+            {
+              name: "steam",
+              url: "https://store.steampowered.com",
+              icon: "brand-steam",
+              icon_color: palette.peach,
+            },
+            {
+              name: "epicgames",
+              url: "https://store.epicgames.com",
+              icon: "brand-fortnite",
+              icon_color: palette.red,
+            },
+            {
+              name: "nintendo",
+              url: "https://store.nintendo.com",
+              icon: "device-nintendo",
+              icon_color: palette.blue,
+            },
+          ],
+        },
+        {
+          name: "video",
+          links: [
+            {
+              name: "gate",
+              url: "https://www.fmovies.gd/tv/63663",
+              icon: "movie",
+              icon_color: palette.green,
+            },
+            {
+              name: "black clover",
+              url: "https://veloratv.ru/watch/tv/73223/1/1",
+              icon: "clover",
+              icon_color: palette.blue,
+            },
+            {
+              name: "youtube",
+              url: "https://www.youtube.com",
+              icon: "brand-youtube",
+              icon_color: palette.peach,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+const CONFIG = new Config(default_configuration, palette);
+
+const root = document.querySelector(":root");
+root.style.setProperty("--bg", palette.mantle);
+root.style.setProperty("--accent", palette.green);
